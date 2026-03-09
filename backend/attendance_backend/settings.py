1307c0463887ad2e56a1d298f1a6ab301807999a
Nandini <nandiniravulapelli@gmail.com>
2026-03-09 23:04:53 +0530

    remove superuser code

--- backend/attendance_backend/settings.py
+++ backend/attendance_backend/settings.py
@@ -91,13 +91,3 @@ REST_FRAMEWORK = {
     ),
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@gmail.com",
-        password="admin123"
-    )
