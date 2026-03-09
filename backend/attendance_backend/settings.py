0ac89c4c6e64f1e076758f5c92141899d4a3946f
Nandini <nandiniravulapelli@gmail.com>
2026-03-09 18:57:08 +0530

    connect postgres database

--- backend/attendance_backend/settings.py
+++ backend/attendance_backend/settings.py
@@ -61,10 +61,11 @@ WSGI_APPLICATION = 'attendance_backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600
     )
 }
+
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
